079e0a7d1dee3eb1b12f7b5d873321f8eb744b44
Andrew <drew.m2340@gmail.com>
2023-06-08 22:39:42 -0400

    Tested json parsing and database is accepting data

--- AircraftDataImporter.py
+++ AircraftDataImporter.py
@@ -1,20 +1,22 @@
 import pymysql, json, requests
 
-aircraftJsonUrl = 'https://192.168.0.230:8080/data/aircraft.json'
+aircraftJsonUrl = 'http://192.168.0.230:8080/data/aircraft.json'
 
-aircraftJsonData = json.loads(requests.get(url = aircraftJsonUrl).text)['results']
+aircraftJsonData = json.loads(requests.get(url = aircraftJsonUrl).text)
 
-connection = pymysql.connect(host = 'localhost', user = 'AircraftDataImporter', passwd = 'dataImporter', db = 'AircraftDB')
+connection = pymysql.connect(host = 'localhost', user = 'AircraftDataImporter', passwd = 'dataImporter', db = 'aircraftdb')
 
 cursor = connection.cursor()
 
-for i, item in enumerate(aircraftJsonData):
-    timestamp = item.get("now", None)
-    icao = item.get("aircraft.hex", None)
-    category = item.get("aircraft.category", None)
-    altitude = item.get("aircraft.alt_baro", None)
-    lat = item.get("aircraft.lat", None)
-    lon = item.get("aircraft.lon", None)
+
+
+for item in aircraftJsonData['aircraft']:
+    timestamp = aircraftJsonData['now']
+    icao = item.get("hex", 0)
+    category = item.get("category", 0)
+    altitude = item.get("alt_baro", 0)
+    lat = item.get("lat", 0)
+    lon = item.get("lon", 0)
 
     cursor.execute("INSERT INTO aircraft_data (timestamp, icao, category, altitude, lat, lon) VALUES (%s, %s, %s, %s, %s, %s)", (timestamp, icao, category, altitude, lat, lon))
 
